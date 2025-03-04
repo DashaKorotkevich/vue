28a9a93540f30ee35542c6e07f678bc57a776135
DashaKorotkevich <dashakorotkevich6878@gmail.com>
2025-03-04 21:18:36 +0300

    feat: deleting post

---
+++ src/components/PostForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <form @submit.prevent>
+      <my-input 
+        v-model="post.title"
+        placeholder= "название"
+        type="text"/> 
+      <my-input 
+        v-model="post.body"  
+        placeholder= "описание" 
+        type="text"/>
+      <my-button @click="createPost"
+      style="align-self: flex-end;"
+      >Создать</my-button>
+    </form>
+</template>
+
+<script> 
+  export default{
+    data() {
+      return {
+        post: {
+          title: '',
+          body: '' 
+        }
+      }
+    },
+    methods: {
+      createPost(){
+         this.post.id= Date.now();
+        this.$emit('create', this.post)
+        this.post = {
+          title: '',
+          body: ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+form{
+  display: flex;
+  flex-direction: column;
+}
+</style>
